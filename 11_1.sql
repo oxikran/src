a51bc34dfed06c175a68c2c2cb0cdedd07529765
oxikran <oxikran@gmail.com>
2015-03-04 14:21:44 +0100

    1º hecho

--- 11_1.sql
+++ 11_1.sql
@@ -3,3 +3,11 @@ entrada (siendo un entero) un valor null devuelva el valor “no definido”
 y si no es null devuelva el valor recibido. Utilizar esta función visualizando 
 de la tabla discos el código, nombre y cantidadMinima. Si no tienes ninguna cantidad 
 minima a null modifica algún registro para que lo puedas probar. */
+ create function f_cantidadinfinita
+ (@cantidad int)
+ returns int
+ begin
+	if @cantidad is null
+	set @cantidad = 'no hay'
+	return @cantidad
+end
