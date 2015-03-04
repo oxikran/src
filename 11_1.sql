cf03fdab0a1c94a012af98c2bbff69852a0f6285
oxikran <oxikran@gmail.com>
2015-03-04 14:14:35 +0100

    t

---
+++ 11_1.sql
@@ -0,0 +1,5 @@
+/*1. Hacer una función escalar que en caso de recibir como parámetro de 
+entrada (siendo un entero) un valor null devuelva el valor “no definido” 
+y si no es null devuelva el valor recibido. Utilizar esta función visualizando 
+de la tabla discos el código, nombre y cantidadMinima. Si no tienes ninguna cantidad 
+minima a null modifica algún registro para que lo puedas probar. */
