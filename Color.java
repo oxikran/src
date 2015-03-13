3a8c472e31e4e5c683d97ec0d450a62d5217b027
oxikran <oxikran@gmail.com>
2015-03-13 20:45:54 +0100

    ok

--- Color.java
+++ Color.java
@@ -1,5 +1,4 @@
-//clase color con los colores get color  // color.rojo ................ to string equals constructor privado
-public class ejercicio implements Comparable<ejercicio>{
+public class Color implements Comparable<Color>{
 	protected static final int ROJO=0;
 	protected static final int NARANJA=1;
 	protected static final int AMARILLO=2;
@@ -15,10 +14,36 @@ public class ejercicio implements Comparable<ejercicio>{
 	public static final Color azul= new Color(AZUL);
 	public static final Color violeta= new Color(VIOLETA);
 	public static final Color rosa= new Color(ROSA);
+	private static final int Color = 0;
+	
+	private Object color;
 	
 	protected Color(int color);{
 		this.color= color;
 	}
-}
+
 	@Override
-	
+	public boolean equals (object obj){
+		if (obj instanceof Color){
+			color otro=
+		}
+	}
+	public Color getColor(){
+		return this;
+	}
+	/*public int compareTo(Color arg0){
+		return color-arg0.color:
+		if (color<arg0.color){
+			return -1;
+		}else if(color==arg0.color){
+			return 0;
+		}else{
+			return 1;
+		}
+			
+	}*/
+	@Override
+	public int compareTo(Color arg0) {
+		return Color-arg0.Color:
+	}
+}
