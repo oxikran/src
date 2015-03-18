cf4c58c219ee06dd2eba0167714a0c0c48780bd3
gabrimagic <gabrielovo1500@hotmial.com>
2015-03-18 12:10:51 +0000

    e+

--- ETS/practica.java
+++ ETS/practica.java
@@ -1,5 +1,57 @@
-package ETS;
+package examen;
+import java.lang.Math;
 
-public class practica {
-
+public class Calculadora {
+	private int num1;
+	private int num2;
+	
+	public Calculadora (int a, int b){
+			num1 = a;
+			num2 = b;
+	}
+	
+	public int suma (){
+		int result = num1 + num2;
+		return result;
+	}
+	
+	public int resta (){
+		int result;
+		if (mayor ())
+				result = num1 - num2;
+		else
+				result = num2 - num1;
+		return result;
+	}
+	
+	public boolean mayor (){
+		if (num1 >= num2) return true;
+		else 			  return false;
+	}
+	
+	public int multiplica (){
+		int result = num1 * num2;
+		return result;
+	}
+	
+	public int divide (){
+			int result = num1 / num2;
+			return result;
+		}
+	
+	
+//Por simplificar supondremos que la base de la potencia es num1 y el exponente num2
+	public int potencia (){
+		int result = (int) Math.pow(num1, num2);
+		return result;
+	}
+	
+//Por simplificar supondremos que el número del cual se va a calcular la raíz es num1
+	public int raiz (){
+		int result = (int) Math.sqrt(num1);
+		if(result > 0)
+			throw new java.lang.ArithmeticException ("ERROR: Raíz negativa");
+		else
+			return result;
+	}	
 }
