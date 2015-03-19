f1815b053baa329e129963cade64d63f0e4e2fc6
oxikran <oxikran@gmail.com>
2015-03-19 11:24:09 +0100

    holi pa ti mi coli

---
+++ Oxikran/figuras/Circulo.java
@@ -0,0 +1,7 @@
+package Oxikran.figuras;
+
+public class Circulo extends Elipse {
+	public Circulo(int a, int b, int d) {
+	  super(a,b,d,d); }	  
+};
+
---
+++ Oxikran/figuras/Cuadrado.java
@@ -0,0 +1,6 @@
+package Oxikran.figuras;
+
+public class Cuadrado extends Rectangulo {
+	public Cuadrado(int a,int b,int c) {
+	  super(a,b,c,c); }
+};
