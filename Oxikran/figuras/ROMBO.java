280bc2fdbd0d51c0cbffaf416593e5c28c60ec0e
oxikran <oxikran@gmail.com>
2015-03-20 13:21:45 +0100

    hgjk,huyi

--- Oxikran/figuras/ROMBO.java
+++ Oxikran/figuras/ROMBO.java
@@ -4,39 +4,43 @@ import java.awt.Graphics;
 import java.awt.Color;
 
 public class ROMBO extends Figura {
-	protected int D, d;
-	public ROMBO(int a, int b) {
+	protected int Dr, dr;
+	public ROMBO(int a, int b, int c, int d) {
 		
 		super(a, b);
-		D=a;
-		d=b;
+		Dr=c;
+		dr=d;
 		
 	}
 
 
-	public float area() {return D*d/2;}
+	public float area() {return Dr*dr/2;}
 
 	
 	public float perimetro() {
 		double l;
-		l=Math.sqrt(Math.pow(D, 2)+Math.pow(d, 2));
+		l=Math.sqrt(Math.pow(Dr, 2)+Math.pow(dr, 2)/2);
 		return (float) Math.rint(4*l);}
 
 	
 	public int alto() {
 		
-		return 0;
+		return Dr;
 	}
 
 	
 	public int ancho() {
 		// TODO Auto-generated method stub
-		return 0;
+		return dr;
 	}
 
 
 	public void dibujar(Graphics g) {
-		// TODO Auto-generated method stub
+		Color cActual = g.getColor();
+		int[] yPuntos ={posicion.y,
+						posicion.y+dr, posicion.y, posicion.y-dr};
+		int[] xPuntos = {posicion.x , posicion.x+Dr, posicion.x, posicion.x-Dr};
+		
 		
 	}
 
